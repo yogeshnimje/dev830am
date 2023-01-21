479d18cf03c54b3a80af8cd0e8ed1603b4877d94
yogeshnimje <yogesh.nimje2594@gmail.com>
2023-01-21 13:45:13 +0000

    code added to dev branch

---
+++ sample.java
@@ -0,0 +1,10 @@
+class Demo
+{
+public static void main(String[] args)
+{
+System.out.println("This code is from dev branch");
+System.out.println("This code is from dev branch");
+System.out.println("This code is from dev branch");
+System.out.println("This code is from dev branch");
+}
+}
