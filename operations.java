8325255e897b32a03a176a83d464e3f4b74c5076
yogeshnimje <yogesh.nimje2594@gmail.com>
2023-01-22 10:24:42 +0000

    add(), sub() operation added

---
+++ operations.java
@@ -0,0 +1,20 @@
+class Opertaion
+{
+public static void main(String[] args)
+{
+public void add()
+{
+int a=5;
+int b=10;
+c=a+b;
+System.out.println("addition is: "+c);
+}
+public void substract()
+{
+int a=10;
+int b=5;
+c=a-b;
+System.out.println("substraction is: "+c);
+}
+}
+}
